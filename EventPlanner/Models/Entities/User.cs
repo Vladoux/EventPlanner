663991eb5a8beeab3bbcc55e7793d65c959faa08
Vladislav Leuko <vladonthebest@gmail.com>
2024-12-08 20:55:49 +0300

    fix: user fields

--- EventPlanner/Models/Entities/User.cs
+++ EventPlanner/Models/Entities/User.cs
@@ -8,8 +8,8 @@ public class User
     public UserRole Role { get; set; }
     public bool IsEmailConfirmed { get; set; } 
     public string? EmailConfirmationToken { get; set; } 
-    public string FullName { get; set; } 
-    public string PhoneNumber { get; set; } 
-    public string Address { get; set; } 
-    public  List<Event> Events { get; set; } 
+    public string? FullName { get; set; } 
+    public string? PhoneNumber { get; set; } 
+    public string? Address { get; set; } 
+    public  List<Event>? Events { get; set; } 
 }
